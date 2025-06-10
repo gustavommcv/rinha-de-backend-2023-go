9db5579acf243a6c3b89ba938d341e10ed907c8b
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-06-09 23:24:50 -0300

    Postgres connection with pgx

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
-func main() {
-	http.HandleFunc("/hello", helloHandler)
+var greeting string
 
+func main() {
+	connString := fmt.Sprintf("user=postgres password=password host=localhost port=5432 dbname=rinhadb")
+
+	conn, err := pgx.Connect(context.Background(), connString) 
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	http.HandleFunc("/hello", helloHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "%s", greeting)
 }
